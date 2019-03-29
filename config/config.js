1303a28921681d1ffb2e8bb8d9b2d122686c8f27
Choi Kyuhyeon <bluejet1104@gmail.com>
2019-03-29 15:48:32 +0900

    아무말

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
 	server_port: 8080,
-	db_url: 'mongodb://localhost:27017/local',
+	db_uri: 'mongodb://localhost:27017/local',
 	db_schemas: [
 	    {file:'./user_schema', collection:'users6', schemaName:'UserSchema', modelName:'UserModel'}
     , {file:'./device_schema', collection:'devices', schemaName:'DeviceSchema', modelName:'DeviceModel'}
